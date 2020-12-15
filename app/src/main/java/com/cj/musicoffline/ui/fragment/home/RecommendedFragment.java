f3e04fd4c1be554beb3ad55303d714b4af06b999
vietle2909 <vietle2809@gmail.com>
2020-12-15 17:22:09 +0700

    Merge remote-tracking branch 'origin' into vietdev
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/cj/musicoffline/ui/fragment/home/RecommendedFragment.java
@@ -0,0 +1,23 @@
+package com.cj.musicoffline.ui.fragment.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.cj.musicoffline.R;
+
+public class RecommendedFragment extends Fragment {
+    private View mView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_recommended, container, false);
+        return mView;
+    }
+}
